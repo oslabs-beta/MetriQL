c19c3c4538d6db24371faf454dfde44cdb3efb11
Diana Li <dianali9619@gmail.com>
2022-03-07 11:44:16 -0500

    add components

---
+++ client/components/QueryInput.js
@@ -0,0 +1,9 @@
+function QueryInput() {
+    return (
+      <div>
+          <h3>Query input here</h3>
+      </div>
+    )
+  }
+  
+  export default QueryInput;
---
+++ client/components/Result.js
@@ -0,0 +1,9 @@
+function Result() {
+    return (
+      <div>
+          <h3>Result of query here</h3>
+      </div>
+    )
+  }
+  
+  export default Result;
