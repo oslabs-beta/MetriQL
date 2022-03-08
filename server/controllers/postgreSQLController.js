81097f9c83547c22bea84d350bb05c0b89f8e532
Diana Li <dianali9619@gmail.com>
2022-03-07 22:23:26 -0500

    server render tables from PG URI

---
+++ server/controllers/postgreSQLController.js
@@ -0,0 +1,36 @@
+const { Pool } = require("pg");
+const CryptoJS = require("crypto-js");
+require('dotenv').config();
+const fs = require('fs')
+const pgQuery = fs.readFileSync('server/query/tables.sql', 'utf8')
+
+const PG_URI_STARWARS = process.env.PG_URI_STARWARS;
+
+const decryptURI = (encryptedUserURI) => {
+    const data = CryptoJS.AES.decrypt(encryptedUserURI, process.env.SECRETKEY);
+    const decryptedURI = data.toString(CryptoJS.enc.Utf8);
+    return decryptedURI;
+}
+
+const postgreSQLController = {};
+
+postgreSQLController.table = async (req, res, next) => {
+    let postURI;
+
+    req.body.uri ? (postURI = decryptURI(req.body.uri)) : (postURI = PG_URI_STARWARS)
+
+    const db = new Pool({ connectionString: postURI });
+    try {
+        const result = await db.query(pgQuery);
+        res.locals.schema = result.rows[0].tables;
+        next();
+    } catch (err) {
+        return next({
+            log: `Error occurred in postgreSQLController.getSchema ERROR: ${err}`,
+            message: { err: 'Error occured in postgreSQLController.getSchema. Check server log for more detail'},      
+        })
+    }
+}
+
+
+module.exports = postgreSQLController
