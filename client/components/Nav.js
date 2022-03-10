408e5366fdd0d236c35448517fd917a9cb6b8c8b
Alfonso Zamarripa <65785328+alfonsozam93@users.noreply.github.com>
2022-03-10 12:43:14 -0500

    Revert "Eric/oauth1"

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,13 +1,30 @@
-// Whenever we create a component that needs access to state, we must import useContext from react
-// we also need to import {GlobalContext, GlobalDispatch} from '/context/global-context' for the state and the dispatch functions respectively
 import React, { useContext, useState } from 'react';
 import {GlobalContext, GlobalDispatch} from '../context/global-context';
 import GlobalState from '../context/GlobalState';
 import {LOGIN, LOGOUT} from '../context/global-actions';
 import Link from 'next/link'
-import UserProfile from './UserProfile';
 
 function Nav () {
+
+  const {state, login, logout} = useContext(GlobalContext);
+  const {dispatch} = useContext(GlobalDispatch);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function onChangeHandler(e) {
+    e.preventDefault;
+    e.target.name === 'username' ? setUsername(e.target.value) : setPassword(e.target.value);
+  }
+
+  function onSubmitHandler(e) {
+      e.preventDefault;
+      login(username, password);
+  }
+
+  console.log('state ', state);
+  console.log('dispatch ', dispatch);
+
 
   return (
     <>
@@ -22,9 +39,18 @@ function Nav () {
             <li>
                 <Link href='/main'>Tool</Link>
             </li>
-            <UserProfile />
         </ul>
     </nav>
+
+    {
+    state.isLoggedIn ? 
+        <button onClick={()=>logout()}>Log Out</button> : 
+        <form>
+            <input type='text' name='username' placeholder='username' onChange={onChangeHandler}></input>
+            <input type='text' name='password' placeholder='password' onChange={onChangeHandler}></input>
+            <button type='button' onClick={onSubmitHandler}>Log In</button>
+        </form>
+    }
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,8 @@
-import LoginModal from '../client/components/LoginModal';
-import { useContext, useState } from 'react';
-import {GlobalContext, GlobalDispatch} from '../client/context/global-context';
-
 function HomePage() {
-
-  const {state} = useContext(GlobalContext);
-  const {dispatch} = useContext(GlobalDispatch);
-
-  const [showModal, toggleShowModal] = useState(false);
-
-  const {isLoggedIn} = state;
-
-  function handleClick() {
-    if (isLoggedIn) {
-      console.log('Logged In, proceed to main page!');
-    } else {
-      toggleShowModal(true);
-    }
-  }
-
     return (
-    <>
-      {/* this text/component should also be blurred when modal pops up, see below */}
+    <div>
       <h1>Welcome to MetriQL!</h1>
-
-      {showModal && <LoginModal />}
-
-      {/* following 2 lines of code are from giraffeql/pages/index.js line 84 and 86 */}
-      <div id='preventClick' onClick={() => toggleShowModal(false)} style={{width: '100vw', height: '100vh', position: 'fixed', zIndex: `${showModal ? '50' : '-10'}`, backgroundColor: `${showModal ? 'rgba(0,0,0,.25)' : 'transparent'}`}} />
-      <div id='blur' style={{filter: `${showModal ? 'blur(5px)' : ''}`}}>
-        (When you click the "Get Started" button, a modal will pop up, and any page content inside this div will get blurred)
-        <div>
-          <button onClick={handleClick}>Get Started</button>
-        </div>
-      </div>
-    </>
+    </div>
     )
   }
   
