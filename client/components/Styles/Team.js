495b750faf4b096e770f791ac6838cade6bdba32
Diana Li <dianali9619@gmail.com>
2022-03-16 11:54:43 -0400

    team photo

--- client/components/Styles/Team.js
+++ client/components/Styles/Team.js
@@ -7,15 +7,16 @@ import LinkedinIcon from './assets/LinkedinIcon';
 const Team = (props) => {
     return (
         <div>
-            <div class="bg-gradient-to-br from-gray-500 to-gray-800 max-w-10xl mx-auto py-28">
+            <div class="relative bg-primary-500 text-gray-100 -mb-8 -mx-8 px-8 py-20 lg:py-24">
                 <div class="container flex justify-center mx-auto pt-12">
                     <div>
-                        <h1 class="xl:text-4xl text-3xl text-center text-white font-bold mx-auto">Meet the Team</h1>
+                        <h5 class="font-bold text-primary-500">Our Team</h5>
+                        <h1 class="text-4xl sm:text-5xl font-black tracking-wide text-center">Meet the Team</h1>
                     </div>
                 </div>
-                <div class = "flex items-center justify-center shrink flex-wrap">
-                    <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs m-8 bg-gray-200">
-                        <img src="./assets/circle.png" alt="Alfonso Zamarripa" width="100" height="100" className="mb-3 w-32 h-32 rounded-full mx-auto"></img>
+                <div class = "flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto">
+                    <div class="mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center">
+                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHw1txEKIYo7w/profile-displayphoto-shrink_200_200/0/1553035247275?e=1652918400&v=beta&t=w4sJfcAwdAmWWVkyz-BG0SF-qBT1_0n4LItgvGJh2jk" alt="Alfonso Zamarripa" width="100" height="100" className="w-64 h-64 bg-contain bg-center rounded"></img>
                         <h1 class="text-lg text-gray-700"> Alfonso Zamarripa </h1>
                         <h3 class="text-sm text-gray-600 "> Software Engineer </h3>
                         <div class="w-full flex justify-center pt-5 pb-5">
@@ -27,8 +28,8 @@ const Team = (props) => {
                             </Link>
                         </div>
                     </div>
-                    <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs m-8 bg-gray-200">
-                        <img src="./assets/circle.png" alt="Diana Li" width="100" height="100" className="mb-3 w-32 h-32 rounded-full mx-auto"></img>
+                    <div class="mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center">
+                        <img src="https://media-exp1.licdn.com/dms/image/C5603AQHE8N7n8Jr0Xw/profile-displayphoto-shrink_800_800/0/1642965707789?e=1652918400&v=beta&t=ZFhwgZT84clFfE8tiIJ-g0gwY6Kz2zgvUgkhMAw61OY" alt="Diana Li" width="100" height="100" className="w-64 h-64 bg-contain bg-center rounded"></img>
                         <h1 class="text-lg text-gray-700"> Diana Li </h1>
                         <h3 class="text-sm text-gray-600 "> Software Engineer </h3>
                         <div class="w-full flex justify-center pt-5 pb-5">
@@ -40,8 +41,8 @@ const Team = (props) => {
                             </Link>
                         </div>
                     </div>
-                    <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs m-8 bg-gray-200">
-                        <img src="./assets/circle.png" alt="Eric Rodgers" width="100" height="100" className="mb-3 w-32 h-32 rounded-full mx-auto"></img>
+                    <div class="mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center">
+                        <img src="https://media-exp1.licdn.com/dms/image/D4D35AQF0Xy2nHoeIlA/profile-framedphoto-shrink_200_200/0/1633641299793?e=1647532800&v=beta&t=YJVVOLxH2FMLtN61Lg8zZIgpH9pTA2CFAlsc-tEO7mg" alt="Eric Rodgers" width="100" height="100" className="w-64 h-64 bg-contain bg-center rounded"></img>
                         <h1 class="text-lg text-gray-700"> Eric Rodgers </h1>
                         <h3 class="text-sm text-gray-600 ">Software Engineer </h3>
                         <div class="w-full flex justify-center pt-5 pb-5">
@@ -53,8 +54,8 @@ const Team = (props) => {
                             </Link>
                         </div>
                     </div>
-                    <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs m-8 bg-gray-200">
-                        <img src="./assets/circle.png" alt="Raymond Huang" width="100" height="100" className="mb-3 w-32 h-32 rounded-full mx-auto"></img>
+                    <div class="mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center">
+                        <img src="https://media-exp1.licdn.com/dms/image/C5603AQFPB4oIfZ-IxQ/profile-displayphoto-shrink_800_800/0/1525638728139?e=1652918400&v=beta&t=wpKfkaZrKp9j-KB367fQPGiEqD8hLQ8JGj88-e0gohI" alt="Raymond Huang" width="100" height="100" className="w-64 h-64 bg-contain bg-center rounded"></img>
                         <h1 class="text-lg text-gray-700"> Raymond Huang </h1>
                         <h3 class="text-sm text-gray-600 "> Software Engineer </h3>
                         <div class="w-full flex justify-center pt-5 pb-5">
@@ -66,8 +67,8 @@ const Team = (props) => {
                             </Link>
                         </div>
                     </div>
-                    <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs m-8 bg-gray-200">
-                        <img src="./assets/circle.png" alt="Rehema Armorer" width="100" height="100" className="mb-3 w-32 h-32 rounded-full mx-auto"></img>
+                    <div class="mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center">
+                        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQE99sLMyKpDqA/profile-displayphoto-shrink_800_800/0/1517072415081?e=1652918400&v=beta&t=XG12PkGgbdZaqCIm6DrPRWECRAZyg4C4oACqk-nfusE" alt="Rehema Armorer" width="100" height="100" className="w-64 h-64 bg-contain bg-center rounded"></img>
                         <h1 class="text-lg text-gray-700"> Rehema Armorer </h1>
                         <h3 class="text-sm text-gray-600 "> Software Engineer </h3>
                         <div class="w-full flex justify-center pt-5 pb-5">
