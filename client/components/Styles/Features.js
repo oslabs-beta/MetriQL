0069307e623a4543d54cf21cce033f625edc6c6e
Diana Li <dianali9619@gmail.com>
2022-03-17 09:31:40 -0400

    landing page

--- client/components/Styles/Features.js
+++ client/components/Styles/Features.js
@@ -12,27 +12,32 @@ const Features = (props) => {
                         <div>
                             <h5 class="font-bold text-purple flex justify-center">Features</h5>
                             <h1 class="text-4xl sm:text-5xl font-black text-blue tracking-wide text-center">GraphQL Tools</h1>
-                            <p class="mt-4 text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary max-w-xl flex justify-center">description</p>
+                            <p class="mt-4 text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary max-w-xl flex justify-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </div>
                     </div>
                     <div class="mt-10 flex flex-col items-center lg:items-stretch lg:flex-row flex-wrap lg:justify-center max-w-screen-lg mx-auto">
-                        <div class="mt-10 flex flex-col items-center lg:items-stretch lg:flex-row flex-wrap lg:justify-center max-w-screen-lg mx-auto">
-                            <a class="flex flex-col items-center text-center h-full mx-4 px-4 py-8 rounded transition-transform duration-300 hover:cursor-pointer transform hover:scale-105">
-                                <span class="mt-4 font-bold text-xl text-blue leading-none">Schema</span>
-                                <p className="mt-4 text-sm font-medium text-secondary">information</p>
-                            </a>
-                            <a class="flex flex-col items-center text-center h-full mx-4 px-4 py-8 rounded transition-transform duration-300 hover:cursor-pointer transform hover:scale-105">
-                                <span class="mt-4 font-bold text-xl text-blue leading-none">Resolve Time Metrics</span>
-                                <p className="mt-4 text-sm font-medium text-secondary">information</p>
-                            </a>
-                            <a class="flex flex-col items-center text-center h-full mx-4 px-4 py-8 rounded transition-transform duration-300 hover:cursor-pointer transform hover:scale-105">
-                                <span class="mt-4 font-bold text-xl text-blue leading-none">Something else</span>
-                                <p className="mt-4 text-sm font-medium text-secondary">information</p>
-                            </a>
+                        <div class ='lg:w-1/3 max-w-xs'>
+                        <a class="flex flex-col items-center text-center h-full mx-4 px-4 py-8 rounded transition-transform duration-300 hover:cursor-pointer transform hover:scale-105">
+                            <span class="mt-4 font-bold text-xl text-blue leading-none">Feature</span>
+                            <p className="mt-4 text-sm font-medium text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </a>
+                        </div>
+                        <div class ='lg:w-1/3 max-w-xs'>
+                        <a class="flex flex-col items-center text-center h-full mx-4 px-4 py-8 rounded transition-transform duration-300 hover:cursor-pointer transform hover:scale-105">
+                            <span class="mt-4 font-bold text-xl text-blue leading-none">Feature</span>
+                            <p className="mt-4 text-sm font-medium text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </a>
+                        </div>
+                        <div class ='lg:w-1/3 max-w-xs'>
+                        <a class="flex flex-col items-center text-center h-full mx-4 px-4 py-8 rounded transition-transform duration-300 hover:cursor-pointer transform hover:scale-105">
+                            <span class="mt-4 font-bold text-xl text-blue leading-none">Feature</span>
+                            <p className="mt-4 text-sm font-medium text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </a>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
--- client/components/Styles/SecondFeature.js
+++ client/components/Styles/SecondFeature.js
@@ -5,33 +5,23 @@ import Link from 'next/link';
 const SecondFeature = (props) => {
   return (
     // container
-    <div class='relative'> 
-    {/* twocolumn */}
+    <div class='relative'>
       <div class='flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-20 md:py-24 items-center'>
-        {/* Column*/}
         <div class='w-full max-w-md mx-auto md:max-w-none md:mx-0'>
-          {/* imageColumn */}
           <div class='md:w-9/12 flex-shrink-0 relative'>
-            {/* imageLink */}
-          <img src='/demophoto.png' alt="MetricQLvisualizer" class="w-5000 h-5000 bg-contain bg-center rounded"></img>
+            <img src='/demophoto.png' alt="MetricQLvisualizer" class="w-5000 h-5000 bg-contain bg-center rounded"></img>
           </div>
         </div>
-          {/* TextColumn */}
-          <div class='md:w-9/12 mt-16 md:mt-0'>
-            {/* TextonLeft */}
-            <div class='md:mr-12 lg:mr-16 md:order-first'>
-              {/* TextContent */}
-              <div class='lg:py-8 text-center md:text-left'>
-                {/* Subheading */}
-                <div class='font-bold text-purple text-center md:text-left'> MetricQL</div>
-                  {/* heading */}
-                <div class='font-black tracking-wide text-center mt-4 text-left text-3xl sm:text-4xl lg:text-5xl text-blue text-center md:text-left leading-tight'> Visualize your MetricQL stuff here!</div>
-                {/* Description */}
-                 <div class='mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary'> MetricQL is so cool and amazing because we all worked on it and copied and pasted hehehehe</div>
-              </div>
+        <div class='md:w-9/12 mt-16 md:mt-0'>
+          <div class='md:mr-12 lg:mr-16 md:order-first'>
+            <div class='lg:py-8 text-center md:text-left'>
+              <div class='font-bold text-purple text-center md:text-left'>Feature 1</div>
+              <div class='font-black tracking-wide text-center mt-4 text-left text-3xl sm:text-4xl lg:text-5xl text-blue text-center md:text-left leading-tight'> Visualize your MetricQL stuff here!</div>
+              <div class='mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
             </div>
           </div>
         </div>
+      </div>
     </div>
   )
 }
