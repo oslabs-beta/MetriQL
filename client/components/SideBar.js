816e9d5e2eda9733c593fd39fc57b0b54719c6d7
Diana Li <dianali9619@gmail.com>
2022-03-30 10:07:48 -0400

    sidebar fixed

--- client/components/SideBar.js
+++ client/components/SideBar.js
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
+// import * as FaIcons from "react-icons/fa";
+// import * as AiIcons from "react-icons/ai";
 import Dialog from "@mui/material/Dialog";
 import { styled } from "@mui/material/styles";
 
@@ -9,103 +9,138 @@ import Schema from "./Schema";
 import classes from "../../styles/SideBar.module.css";
 import { SidebarContext } from "../context/global-context";
 import { initialDisplayState } from "../context/global-reducer";
+import StorageIcon from '@mui/icons-material/Storage';
+import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import SaveAltOutlinedIcon from '@mui/icons-material/SaveAltOutlined';
 
 function SideBar({ openDB }) {
-  const { displayState, displayDispatch, urlState } = useContext(SidebarContext);
+    const { displayState, displayDispatch, urlState } = useContext(SidebarContext);
 
-  const openSidebar = () => {
-    displayDispatch({
-      type: "UPDATE_SIDEBAR_DISPLAY",
-      payload: true,
-    });
-  };
+    // const closeSidebar = () => {
+    //     displayDispatch({
+    //       type: "UPDATE_SIDEBAR_DISPLAY",
+    //       payload: false,
+    //     });
+    //   };
 
-  const closeSidebar = () => {
-    displayDispatch({
-      type: "UPDATE_SIDEBAR_DISPLAY",
-      payload: false,
-    });
-  };
+    const openDBHandler = () => {
+        return openDB();
+    };
 
-  const openDBHandler = () => {
-    return openDB();
-  };
+    const handleSchemaOpen = (value) => {
+        console.log('openplease')
+        displayDispatch({
+            type: "UPDATE_SCHEMA_DISPLAY",
+            payload: value,
+        });
+    };
 
-  const handleSchemaOpen = (value) => {
-    displayDispatch({
-      type: "UPDATE_SCHEMA_DISPLAY",
-      payload: value,
-    });
-  };
+    const handleSchemaClose = () => {
+        console.log('please close')
+        displayDispatch({
+            type: "UPDATE_SCHEMA_DISPLAY",
+            payload: false,
+        });
+    };
 
-  const handleSchemaClose = () => {
-    displayDispatch({
-      type: "UPDATE_SCHEMA_DISPLAY",
-      payload: false,
-    });
-  };
+    const handleSchemaVisOpen = () => {
+        displayDispatch({
+            type: "UPDATE_D3_DISPLAY",
+            payload: true,
+        });
+    };
 
-  const handleSchemaVisOpen = () => {
-    displayDispatch({
-      type: "UPDATE_D3_DISPLAY",
-      payload: true,
-    });
-    console.log("handle open")
-  };
+    const handleSchemaVisClose = () => {
+        displayDispatch({
+            type: "UPDATE_D3_DISPLAY",
+            payload: false,
+        });
+    };
 
-  const handleSchemaVisClose = () => {
-    displayDispatch({
-      type: "UPDATE_D3_DISPLAY",
-      payload: false,
-    });
-  };
+    return (
+            <div>
+                <nav className={classes.sidebar} >
+                    <ul className={classes.mainMenu}>
 
-  return (
-    <div>
-      <nav
-        className={displayState.sidebar ? classes.sidebarActive : classes.sidebar}
-      >
-        <ul className={classes.sideMenu} onClick={closeSidebar}>
-          <li className={classes.sidebarToggle}>
-            <AiIcons.AiOutlineClose className={classes.closeSide} />
-          </li>
-          <li className={classes.navText}>
-            <a onClick={openDBHandler}>DB Entry</a>
-          </li>
-          <li className={classes.navText}>
-            <a onClick={() => handleSchemaOpen("types")}>View Types</a>
-            <Dialog
-              onClose={handleSchemaClose}
-              open={displayState.schema === "types"}
-            >
-              <Schema schema={urlState} view={"types"} />
-            </Dialog>
-          </li>
-          <li className={classes.navText}>
-            <a onClick={() => handleSchemaOpen("resolvers")}>View Resolvers</a>
-            <Dialog
-              onClose={handleSchemaClose}
-              open={displayState.schema === "resolvers"}
-            >
-              <Schema schema={urlState} view={"resolvers"} />
-            </Dialog>
-          </li>
-          <li className={classes.navText}>
-            <a onClick={() => handleSchemaVisOpen("visuals")}>Schema Visualizer</a>
-            <Dialog
-              onClose={handleSchemaVisClose}
-              open={displayState.visuals}
-              fullWidth={true}
-              maxWidth={'xl'}
-            >
-              <SchemaVisual visuals={urlState}/>
-            </Dialog>
-          </li>
-        </ul>
-      </nav>
-      <FaIcons.FaBars className={classes.sidebarBar} onClick={openSidebar} />
-    </div>
-  );
+                        <li className={classes.menuItemOne}>
+                            <a onClick={openDBHandler} className={classes.menuA}>
+                            <StorageIcon style={{fill: '#6415FF'}}/>
+                                <div className={classes.menuTxtHld}>
+                                    <i className={classes.menuIcon}></i>
+                                    <span className={classes.menuTxt}>Change Database</span>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li className={classes.menuItem}>
+                            <a onClick={() => handleSchemaOpen("types")} className={classes.menuA}>
+                                <FeaturedPlayListIcon style={{fill: '#6415FF'}}/>
+                                <div className={classes.menuTxtHld}>
+                                    <i className={classes.menuIcon} class="fa=solid fa-igloo"></i> 
+                                    <span className={classes.menuTxt}>View Types</span>
+                                </div>
+                                
+                            </a>
+                            <Dialog
+                                    onClose={handleSchemaClose}
+                                    open={displayState.schema === "types"}
+                                >
+                                    <Schema schema={urlState} view={"types"} />
+                                </Dialog>
+                        </li>
+
+                        <li className={classes.menuItem}>
+                            <a onClick={() => handleSchemaOpen("resolvers")} className={classes.menuA}>
+                                <FeaturedPlayListIcon style={{fill: '#6415FF'}}/>
+                                <div className={classes.menuTxtHld}>
+                                    <i className={classes.menuIcon}></i>
+                                    <span className={classes.menuTxt}>View Resolvers</span>
+                                </div>
+                                
+                            </a>
+                            <Dialog
+                                    onClose={handleSchemaClose}
+                                    open={displayState.schema === "resolvers"}
+                                >
+                                    <Schema schema={urlState} view={"resolvers"} />
+                                </Dialog>
+                        </li>
+
+                        <li className={classes.menuItem}>
+                            <a onClick={() => handleSchemaVisOpen("visuals")} className={classes.menuA}>
+                               <InsertPhotoIcon style={{fill: '#6415FF'}}/>
+                                <div className={classes.menuTxtHld}>
+                                    <i className={classes.menuIcon}></i>
+                                    <span className={classes.menuTxt}>Visualizer</span>
+                                </div>
+                                
+                            </a>
+                            <Dialog
+                                    onClose={handleSchemaVisClose}
+                                    open={displayState.visuals}
+                                    fullWidth={true}
+                                    maxWidth={'xl'}
+                                >
+                                    <SchemaVisual visuals={urlState} />
+                                </Dialog>
+                        </li>
+
+                        <li className={classes.menuItem}>
+                            <a href="https://github.com/oslabs-beta/MetriQL" className={classes.menuA} target='_blank'>
+                               <SaveAltOutlinedIcon style={{fill: '#6415FF'}}/>
+                                <div className={classes.menuTxtHld}>
+                                    <i className={classes.menuIcon}></i>
+                                    <span className={classes.menuTxt}>Download</span>
+                                </div>
+                            </a>
+                        </li>
+
+                    </ul>
+                </nav>
+            </div>
+       
+    );
 }
 
 export default SideBar;
